a9b4fa3738a23525c835a38ae288f89ca13e6ad4
MlgEpicCar <115513939+MlgEpicCar@users.noreply.github.com>
2024-10-09 14:38:43 -0400

    added stuff

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.getElementById("h1").textContent = `Trip Destinaton Check List`;
+
+img = document.createElement("img");
+img.src = "/images/gibraltar.jpg";
+src = document.getElementById("img");
+src.appendChild(img);
