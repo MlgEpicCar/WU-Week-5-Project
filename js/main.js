9d1b3a08b72856f16431aa25374d6a4650f1cca4
MlgEpicCar <115513939+MlgEpicCar@users.noreply.github.com>
2024-10-10 13:07:33 -0400

    css

--- js/main.js
+++ js/main.js
@@ -4,3 +4,29 @@ img = document.createElement("img");
 img.src = "/images/gibraltar.jpg";
 src = document.getElementById("img");
 src.appendChild(img);
+
+function list() {
+
+    var itemsAvailable = document.getElementById('div-list');
+    var checks = new Array();
+    var checks = [" Food", " Water", " Jacket for Winter", " Clothes", " Phone", " Luggage", " Money", " Wallet", " Car for Transportation" ];
+    
+
+    var item, p, br;
+
+   for(var count in checks)
+    {
+      item = document.createElement("input");   
+      item.value=(checks[count] + '</br>');
+      item.type="checkbox";
+      item.id="item" + count;
+      p = document.createElement("span");
+      p.innerHTML = checks[count] + ": ";
+      br = document.createElement("br");
+      itemsAvailable.appendChild(item);
+      itemsAvailable.appendChild(p);
+      itemsAvailable.appendChild(br);
+   }
+}
+
+list();
